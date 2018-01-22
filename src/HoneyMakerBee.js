9c665e1a948c79cc1b7d01ddbe267048c8ddea50
Whitney Seiler <whitney.seiler@gmail.com>
2018-01-22 11:35:06 -0800

    Pass all BeesBeesBees tests

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,19 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.color = 'yellow';
+  this.honeyPot = 0;
+  this.job = 'make honey'
 };
 
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function(){
+  this.honeyPot++;
+}
+
+HoneyMakerBee.prototype.giveHoney = function(){
+  this.honeyPot -= 1;
+  return this.honeyPot;
+}
